e8849184ce4f83ce41bf5f08ec64f01071650e2a
dannyelgjl <dannyelgjl@gmail.com>
2020-10-17 09:07:34 -0300

    implementando react navigation

---
+++ mobile/src/pages/OrphanagesDetails/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+export default function OrphanageDetails () {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>
+        Teste
+      </Text>
+    </View>
+  );
+}
---
+++ mobile/src/pages/OrphanagesMap/index.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
+
+import { Feather } from '@expo/vector-icons';
+
+import mapMarker from '../../assets/images/map-marker.png';
+
+import styles from'./styles';
+import { useNavigation } from '@react-navigation/native';
+
+export default function OrphanagesMap() {
+  const navigation = useNavigation();
+
+  const handleNavigationOrphanageDetails = () => {
+    navigation.navigate('OrphanageDetails');
+  }
+
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold, 
+    Nunito_700Bold, 
+    Nunito_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+    <MapView  
+      style={styles.map} 
+      provider={PROVIDER_GOOGLE}
+      initialRegion={{  
+        latitude: -7.1164938,
+        longitude: -34.8276623,
+        latitudeDelta: 0.008,
+        longitudeDelta: 0.008,
+      }} >
+        <Marker 
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: -7.1164938,
+            longitude: -34.8276623,
+          }}
+        >
+          <Callout tooltip={true} onPress={handleNavigationOrphanageDetails} >
+            <View style={ styles.calloutContainer } >
+              <Text style={ styles.calloutText } >Lar das Meninas</Text>
+            </View>
+          </Callout>
+       </Marker>   
+     </MapView>   
+
+     <View style={styles.footer}>
+          <Text style={styles.footerText}>
+            2 orfanatos encontrados
+          </Text>
+
+          <TouchableOpacity  style={styles.createOphanageButton} onPress={() => {}} >
+            <Feather  name="plus" size={20} color="#fff" />
+          </TouchableOpacity>
+     </View>
+  </View>
+  );
+}
