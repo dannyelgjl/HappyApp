16a901b094ba973c83ddd134eff9863a17d096a2
dannyelgjl <dannyelgjl@gmail.com>
2020-10-16 23:49:17 -0300

    adicionando marcador no mapa

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+
+import mapMarker from './src/assets/images/map-marker.png'
 
 export default function App() {
   return (
@@ -14,7 +16,15 @@ export default function App() {
           longitude: -34.8276623,
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
-        }} />
+        }} >
+          <Marker 
+            icon={mapMarker}
+            coordinate={{
+              latitude: -7.1164938,
+              longitude: -34.8276623,
+            }}
+          />
+       </MapView>   
     </View>
   );
 }
